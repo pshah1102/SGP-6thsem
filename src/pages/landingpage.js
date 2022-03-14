fa089ab9b6c2439b0abd658a47590de501034c87
19IT121 <19it121@charusat.edu.in>
2022-03-14 15:04:13 +0530

    Adding another testing commit

--- src/pages/landingpage.js
+++ src/pages/landingpage.js
@@ -4,7 +4,7 @@ const Landing=()=>{
        <div>This is a landng page
            <h1>19IT133 is making a landing page for our project  </h1>
        <h2> Also commiting the code on github</h2>
-
+    <h2> another commit to check the code changes</h2>
        </div>
        
     )
