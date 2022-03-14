503fbcc9b39132b8d3e38aea3bcc672eba906446
19IT121 <19it121@charusat.edu.in>
2022-03-14 14:58:57 +0530

    Pushing new changes to the gothub repository

--- src/pages/landingpage.js
+++ src/pages/landingpage.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 const Landing=()=>{
     return(
-       <div>This is a landng page</div>
+       <div>This is a landng page
+           <h1>19IT133 is making a landing page for our project  </h1>
+       <h2> Also commiting the code on github</h2>
+
+       </div>
+       
     )
 }
 export default Landing;
